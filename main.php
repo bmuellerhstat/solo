292b7658c7e03cd55306f21f0a945044113503ee
Mr. Mueller <bmueller@hstat.org>
2016-11-15 14:37:23 +0000

    add main

---
+++ main.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+// Read directory, spit out links
+if ($handle = opendir('.')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo '<a href="/'.$entry.'">Link to file '.$entry.'</a><br>';
+        }
+    }
+    closedir($handle);
+}
+?>
+</body>
+</html>
